989c6b2cf90e7ad1f38e7ee6f73acd73eb6ac6b0
Thomas Massmann <thomas.massmann@it-spir.it>
2014-05-15 17:33:25 +0200

    Added custom units for pint (area units).

--- src/z3c/formwidget/multiunit/__init__.py
+++ src/z3c/formwidget/multiunit/__init__.py
@@ -1,1 +1,10 @@
 """A multi unit widget for z3c.form."""
+
+# python imports
+from pint import UnitRegistry
+import os
+
+ureg = UnitRegistry()
+ureg.load_definitions(
+    os.path.join(os.path.dirname(__file__), 'pint_units.txt')
+)
