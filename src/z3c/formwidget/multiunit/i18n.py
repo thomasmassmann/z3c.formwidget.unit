8efe8f935c99fbe3689339145fccd3663995b9ae
Thomas Massmann <thomas.massmann@it-spir.it>
2014-05-15 17:32:23 +0200

    Added i18n message factory.

---
+++ src/z3c/formwidget/multiunit/i18n.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""I18N utilities for z3c.formwidget.multiunit."""
+
+# zope imports
+from zope.i18nmessageid import MessageFactory
+
+
+_ = MessageFactory('z3c.formwidget.multiunit')
