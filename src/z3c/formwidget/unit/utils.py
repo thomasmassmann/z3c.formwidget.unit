b66ab28fefe827695ae030c995804a47893401f8
Thomas Massmann <thomas.massmann@it-spir.it>
2014-05-21 17:45:32 +0200

    Fixed unit level selection.

--- src/z3c/formwidget/unit/utils.py
+++ src/z3c/formwidget/unit/utils.py
@@ -9,17 +9,22 @@ def get_best_unit(value, system, dimension, level_min=0, level_max=None):
     """Return the unit that fits best."""
     level = 0
     if dimension == interfaces.DIMENSION_AREA:
-        level = 2
+        level = 0
         if value < 1000:
             level = 0
-        elif value <= 1000000:
+        elif value < 1000000:
             level = 1
+        else:
+            level = 2
     elif dimension == interfaces.DIMENSION_LENGTH:
-        level = 2
+        level = 1
         if value < 0.5:
             level = 0
-        elif value > 400:
-            level = 3
+        elif value < 400:
+            level = 1
+        else:
+            level = 2
+
     level = max(level_min, level)
     if level_max is not None:
         level = min(level_max, level)
